9d228adf04aa813db675fd159a94a5608c39f5fb
thibault <thibault.blanc@yahoo.fr>
2015-12-07 16:38:32 +0100

    implémentation type des colonnes

--- src/test/java/org/opencompare/Statistique4TestThibault.java
+++ src/test/java/org/opencompare/Statistique4TestThibault.java
@@ -65,5 +65,59 @@ public class Statistique4TestThibault {
 		}
 		
 	}
+	
+	@Test
+	public void faitsInteressantsWithTreatment() throws IOException {
+		File directory = new File("pcms2");
+		File[] files = directory.listFiles();
+		//On crée une liste contenant l'ensemble des features
+		List<ArrayList<String>> liste=new ArrayList<ArrayList<String>>();
+		ArrayList<String> listeTemp=null;
+		
+		List<Value> listeInterpretation=null;
+		
 
+		for(File file : files){
+			PCMLoader loader = new KMFJSONLoader();
+
+			List<PCMContainer> pcmContainers = loader.load(file);
+			
+			
+			for (PCMContainer pcmContainer : pcmContainers) {
+				PCM pcm = pcmContainer.getPcm();
+				liste=new ArrayList<ArrayList<String>>();
+				listeInterpretation=new ArrayList<Value>();
+				Cell cell=null;
+				for (Feature feature : pcm.getConcreteFeatures()) {
+					listeTemp=new ArrayList<String>();
+					for (Product product : pcm.getProducts()) {
+						
+
+						// Find the cell corresponding to the current feature and product
+						cell = product.findCell(feature);
+						// Get information contained in the cell
+						String content = cell.getContent();
+						
+						listeTemp.add(content);
+
+
+						// Print the content of the cell
+						//System.out.println("(" + product.getName() + ", " + feature.getName() + ") = " + content);
+
+
+					}
+					listeInterpretation.add(cell.getInterpretation());
+					liste.add(listeTemp);
+				}
+			}
+			System.out.println("change product :"+file.getName());
+			for(int i=0;i<liste.size();i++ ){
+				System.out.println("change feature");
+				for(int j=0;j<liste.get(i).size();j++){
+					System.out.println(liste.get(i).get(j));
+				}
+			}
+		}
+		
+	}
 }
