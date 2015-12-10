64bc77d84cdc430d6315024b637cf0d9ef56398f
jonathan oheix <jonathan.oheix@laposte.net>
2015-12-10 23:45:00 +0100

    Partie stat+++ terminée, tout marche !! :D

--- src/test/java/org/opencompare/Statistiques5Test.java
+++ src/test/java/org/opencompare/Statistiques5Test.java
@@ -1,6 +1,7 @@
 package org.opencompare;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,96 +23,103 @@ public class Statistiques5Test {
 	@Test
 	public void matriceDistances() throws IOException{
 
-		// variable du nombre de lignes du fichier
-		int nbLignes=0; 
+		File directory = new File("pcms");
+		File[] files = directory.listFiles();
 
-		// on crée un tableau listant le type de toutes les variables "quali" ou "quanti"
-		ArrayList<String> types = new ArrayList<String>();
-		boolean isQuanti;
+		for(File file : files){
+			// variable du nombre de lignes du fichier
+			int nbLignes=0; 
 
-		// on crée un tableau listant le nom de toutes les lignes
-		ArrayList<String> noms = new ArrayList<String>();
+			// on crée un tableau listant le type de toutes les variables "quali" ou "quanti"
+			ArrayList<String> types = new ArrayList<String>();
+			boolean isQuanti;
 
-		// importer les données
-		ArrayList<ArrayList> data = new ArrayList<ArrayList>();
-		ArrayList row;
+			// on crée un tableau listant le nom de toutes les lignes
+			ArrayList<String> noms = new ArrayList<String>();
 
-		// on ouvre le fichier
-		File file = new File("pcms/List_of_AMD_graphics_processing_units_10.pcm");
-		PCMLoader loader = new KMFJSONLoader();
-		List<PCMContainer> pcmContainers = loader.load(file);
+			// importer les données
+			ArrayList<ArrayList> data = new ArrayList<ArrayList>();
+			ArrayList row;
 
-		// lecture du fichier
-		for (PCMContainer pcmContainer : pcmContainers) {
-			PCM pcm = pcmContainer.getPcm();
-			Cell cell=null;
+			// on ouvre le fichier
+			PCMLoader loader = new KMFJSONLoader();
+			List<PCMContainer> pcmContainers = loader.load(file);
 
-			// on lit le fichier ligne par ligne
-			for (Product product : pcm.getProducts()) {
-				row = new ArrayList();
-				for (Feature feature : pcm.getConcreteFeatures()) {
+			// lecture du fichier
+			for (PCMContainer pcmContainer : pcmContainers) {
+				PCM pcm = pcmContainer.getPcm();
+				Cell cell=null;
 
-					// Find the cell corresponding to the current feature and product
-					cell = product.findCell(feature);
-					// Get information contained in the cell
-					String content = cell.getContent();
-					//System.out.println(content);
-					row.add(content);
-				}
-				noms.add(product.getName());
-				data.add(row);
-				nbLignes++;
-			}
-		}
-
-		// 2eme lecture du fichier pour les types des variables
-		for (PCMContainer pcmContainer : pcmContainers) {
-			PCM pcm = pcmContainer.getPcm();
-			Cell cell=null;
-
-			// on lit le fichier colonne par colonne
-			for (Feature feature : pcm.getConcreteFeatures()) {
-				isQuanti=true;
+				// on lit le fichier ligne par ligne
 				for (Product product : pcm.getProducts()) {
+					row = new ArrayList();
+					for (Feature feature : pcm.getConcreteFeatures()) {
 
-					// Find the cell corresponding to the current feature and product
-					cell = product.findCell(feature);
-					// Get information contained in the cell
-					Value interpretation = cell.getInterpretation();
-
-					if(!((interpretation instanceof IntegerValue) | (interpretation instanceof RealValue))){
-						isQuanti=false;
+						// Find the cell corresponding to the current feature and product
+						cell = product.findCell(feature);
+						// Get information contained in the cell
+						String content = cell.getContent();
+						//System.out.println(content);
+						row.add(content);
 					}
-
-				}
-				if(isQuanti){
-					types.add("quanti");
-				}
-				else{
-					types.add("quali");
+					noms.add(product.getName());
+					data.add(row);
+					nbLignes++;
 				}
 			}
-		}
 
-		// stocker dans un arraylist(arraylist) les distances entre chaque couple d'individus
-		ArrayList<ArrayList<Double>> distances= new ArrayList<ArrayList<Double>>();
+			// 2eme lecture du fichier pour les types des variables
+			for (PCMContainer pcmContainer : pcmContainers) {
+				PCM pcm = pcmContainer.getPcm();
+				Cell cell=null;
 
-		// on remplit la matrice des distances pour chaque couple de lignes
-		for(int i=0;i<nbLignes;i++){
-			ArrayList<Double> ligne = new ArrayList<Double>();
-			// on compare la ligne i aux autres lignes j
-			for(int j=0;j<nbLignes;j++){
-				// Attribuer les vecteurs de valeurs
-				ArrayList<Value> valeurs1 = new ArrayList<Value>();
-				ArrayList<Value> valeurs2 = new ArrayList<Value>();
-				valeurs1=data.get(i);
-				valeurs2=data.get(j);
-				//System.out.println(distanceIndividus(valeurs1,valeurs2,types));
-				ligne.add(distanceIndividus(valeurs1,valeurs2,types));
+				// on lit le fichier colonne par colonne
+				for (Feature feature : pcm.getConcreteFeatures()) {
+					isQuanti=true;
+					for (Product product : pcm.getProducts()) {
+
+						// Find the cell corresponding to the current feature and product
+						cell = product.findCell(feature);
+						// Get information contained in the cell
+						Value interpretation = cell.getInterpretation();
+
+						if(!((interpretation instanceof IntegerValue) | (interpretation instanceof RealValue))){
+							isQuanti=false;
+						}
+
+					}
+					if(isQuanti){
+						types.add("quanti");
+					}
+					else{
+						types.add("quali");
+					}
+				}
 			}
-			distances.add(ligne);
-		}
 
+			// stocker dans un arraylist(arraylist) les distances entre chaque couple d'individus
+			ArrayList<ArrayList<Double>> distances= new ArrayList<ArrayList<Double>>();
+
+			// on remplit la matrice des distances pour chaque couple de lignes
+			for(int i=0;i<nbLignes;i++){
+				ArrayList<Double> ligne = new ArrayList<Double>();
+				// on compare la ligne i aux autres lignes j
+				for(int j=0;j<nbLignes;j++){
+					// Attribuer les vecteurs de valeurs
+					ArrayList<Value> valeurs1 = new ArrayList<Value>();
+					ArrayList<Value> valeurs2 = new ArrayList<Value>();
+					valeurs1=data.get(i);
+					valeurs2=data.get(j);
+					//System.out.println(distanceIndividus(valeurs1,valeurs2,types));
+					ligne.add(distanceIndividus(valeurs1,valeurs2,types));
+				}
+				distances.add(ligne);
+			}
+			
+			//appeler fonctions pour creer fichiers json et html
+			transformationMatrixToD3js(distances,file.getName(),noms);
+			genererHTML(file.getName());
+		}
 	}
 
 
@@ -156,5 +164,89 @@ public class Statistiques5Test {
 			//System.out.println(1);
 			return 1;
 		}
+	}
+
+	/**
+	 * Fonction pour envoyer une ArrayList<ArrayList<double>> (ce qui correspondra à la matrice de distance
+	 *  dans d3js (format Json spécifique)
+	 */
+
+	public void transformationMatrixToD3js(ArrayList<ArrayList<Double>> array, String nomFichier, ArrayList<String> nom){
+		File f = new File ("fichiersHTML/fichiersJson/"+nomFichier+".json");
+
+		try{
+			FileWriter fw = new FileWriter(f);
+			fw.write("{\r\n\"nodes\":[\r\n");
+			for(int i=0;i<array.size();i++){
+				if(i<array.size()-1){
+					fw.write("{\"name\":\""+nom.get(i)+"\", \"group\":"+i+"},\r\n");
+				}
+				else{
+					fw.write("{\"name\":\""+nom.get(i)+"\", \"group\":"+i+"}\r\n");
+				}
+			}
+			fw.write(" ],\r\n\"links\":[");
+
+			for(int i=0;i<array.size();i++){
+				for(int j=0;j<array.get(i).size();j++){
+					if((i<array.size()-1) | (j<array.get(i).size()-1)){
+						fw.write("{\"source\":"+i+", \"target\":"+j+", \"value\":"+array.get(i).get(j)+"},\r\n");
+					}
+					else{
+						fw.write("{\"source\":"+i+", \"target\":"+j+", \"value\":"+array.get(i).get(j)+"}\r\n");
+					}
+				}
+			}
+			fw.write(" ]\r\n }");
+			fw.close();
+		}
+		catch (IOException exception){
+			System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+		}
+
+	}
+
+	public void genererHTML(String nomFichier){
+		File f = new File ("fichiersHTML/"+nomFichier+".html");
+
+		try{
+			FileWriter fw = new FileWriter(f);
+			// ecriture du fichier HTML
+			fw.write("﻿<!DOCTYPE html>\n<meta charset=\"utf-8\">\n<style>\n\n.node {\n  stroke: #fff;\n  stroke-width: 1.5px;\n}\n\n.link {\n  stroke: #999;\n  stroke-opacity: .6;\n}\n\n</style>\n<body>\n<script src=\"d3.min.js\"></script>\n<script>\n\nvar width = 960,\n    height = 500;\n\nvar color = d3.scale.category20();\n\nvar force = d3.layout.force()\n    .charge(-120)\n    .linkDistance(30)\n    .size([width, height]);\n\nvar svg = d3.select(\"body\").append(\"svg\")\n    .attr(\"width\", width)\n    .attr(\"height\", height);\n\nd3.json(\"fichiersJson/"+nomFichier+".json\", function(error, graph) {\n  if (error) throw error;\n\n  force\n      .nodes(graph.nodes)\n      .links(graph.links)\n      .start();\n\n  var link = svg.selectAll(\".link\")\n      .data(graph.links)\n    .enter().append(\"line\")\n      .attr(\"class\", \"link\")\n      .style(\"stroke-width\", function(d) { return Math.sqrt(d.value); });\n\n  var node = svg.selectAll(\".node\")\n      .data(graph.nodes)\n    .enter().append(\"circle\")\n      .attr(\"class\", \"node\")\n      .attr(\"r\", 5)\n      .style(\"fill\", function(d) { return color(d.group); })\n      .call(force.drag);\n\n  node.append(\"title\")\n      .text(function(d) { return d.name; });\n\n  force.on(\"tick\", function() {\n    link.attr(\"x1\", function(d) { return d.source.x; })\n        .attr(\"y1\", function(d) { return d.source.y; })\n        .attr(\"x2\", function(d) { return d.target.x; })\n        .attr(\"y2\", function(d) { return d.target.y; });\n\n    node.attr(\"cx\", function(d) { return d.x; })\n        .attr(\"cy\", function(d) { return d.y; });\n  });\n});\n\n</script>");
+
+			fw.close();
+		}
+		catch (IOException exception){
+			System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+		}
+	}
+
+	@Test
+	public void test(){
+		/*ArrayList<Double> array1=new ArrayList<Double>();
+		array1.add(1.5);
+		array1.add(2.5);
+		array1.add(3.5);
+		ArrayList<Double> array2=new ArrayList<Double>();
+		array2.add(4.5);
+		array2.add(5.5);
+		array2.add(6.5);
+		ArrayList<Double> array3=new ArrayList<Double>();
+		array3.add(7.5);
+		array3.add(8.5);
+		array3.add(9.5);
+		ArrayList<ArrayList<Double>> array=new ArrayList<ArrayList<Double>>();
+		array.add(array1);
+		array.add(array2);
+		array.add(array3);
+
+		ArrayList<String> nom=new ArrayList<String>();
+		nom.add("paul");
+		nom.add("Jean");
+		nom.add("Alfred");
+
+		this.transformationMatrixToD3js(array, "test", nom);*/
+
+		genererHTML("test");
 	}
 }
